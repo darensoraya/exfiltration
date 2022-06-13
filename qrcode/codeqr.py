5c09813691f7a6905ce8de34000295027de91fa4
darensoraya <daren.ndjomyahoo.fr>
2022-06-13 16:36:13 +0200

    small fixes
    Addressing issue #20 finally. Thanks to @CapacitorSet.

--- qrcode/codeqr.py
+++ qrcode/codeqr.py
@@ -1,18 +1,8 @@
 import qrcode
-from PIL import Image
-from pdf2image import convert_from_path
-from PyPDF2 import PdfFileReader
-
+import cv2
 
 
 def main():
-    # code pour transformer le fichier en png
-    # images = convert_from_path('../encode/Document.pdf.encoded')
-    # for image in images:
-        # image.save('out.png','PNG')
-    # PDF_read = PdfFileReader(content)
-    # page_1 = PDF_read.getPage(0)
-    # print(page_1.extractText())
 
     content = open('../encode/Document.pdf.encoded', 'r')
     j = 0
@@ -22,13 +12,22 @@ def main():
         content = open('../encode/Document.pdf.encoded', 'r')
         qr = qrcode.QRCode()
         qr.add_data(content.read()[i:i+2325])
-        print("okay"+str(j))
+        # print("okay"+str(j))
         qr.make()
         img_qr = qr.make_image()
         img_qr.save("qr_"+str(j)+".png")
         j = j+1
+    print(j)
 
-
+    for i in range(0, j):
+        filename = "qr_"+str(i)+".png"
+        print(i)
+        image = cv2.imread(filename)
+        detector = cv2.QRCodeDetector()
+        data, vertices_array, binary_qrcode = detector.detectAndDecode(image)
+        print(data)
+        with open('qr.pdf.encoded', 'a') as fichier:
+            fichier.write(data)
 
 
 if __name__ == '__main__':
